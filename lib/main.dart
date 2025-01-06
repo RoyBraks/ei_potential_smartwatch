06da31bb4731c564a657036f42315ac90b7a7e35
Roy Braks <roybraks@gmail.com>
2025-01-06 14:23:50 +0100

    added heartrate monitor (unable to test due to lack of hardware)

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
-
+import 'package:health/health.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
 
   @override
   Widget build(BuildContext context) {
@@ -77,10 +84,72 @@ class TimerSection extends StatelessWidget {
   }
 }
 
-class MetricsSection extends StatelessWidget {
+class MetricsSection extends StatefulWidget {
   const MetricsSection({
     super.key,
   });
+
+  @override
+  State<MetricsSection> createState() => _MetricsSectionState();
+}
+
+class _MetricsSectionState extends State<MetricsSection> {
+  Future<void> installHealthConnect() async => await Health().installHealthConnect();
+
+  @override
+  void initState() {
+    super.initState();
+    fetchHeartRate();
+  }
+  
+  int? heartrate = 0;
+
+  Future<void> fetchHeartRate() async {    
+    bool? hasPermissions =
+        await Health().hasPermissions([HealthDataType.HEART_RATE], permissions: [HealthDataAccess.READ_WRITE]);
+
+    hasPermissions = false;
+
+    bool isAuthorized = false;
+    if (!hasPermissions) {
+      // requesting access to the data types before reading them
+      try {
+       isAuthorized = await Health()
+            .requestAuthorization([HealthDataType.HEART_RATE], permissions: [HealthDataAccess.READ_WRITE]);
+      } catch (error) {
+        debugPrint("Exception in authorize: $error");
+      }
+    }
+
+    if (isAuthorized) {
+      try {
+        DateTime now = DateTime.now();
+        DateTime startTime = now.subtract(Duration(hours: 1));
+
+        // Ophalen van hartslaggegevens
+        List<HealthDataPoint> healthData = await Health().getHealthDataFromTypes(
+          startTime: startTime,
+          endTime: now,
+          types: [HealthDataType.HEART_RATE]
+        );
+
+        if (healthData.isNotEmpty) {
+          // Neem de laatste beschikbare waarde
+          setState(() {
+            heartrate = (healthData.last.value as num).round();
+          });
+        } else {
+          setState(() {
+            heartrate = 0;
+          });
+        }
+      } catch (e) {
+        print("Fout bij het ophalen van hartslaggegevens: $e");
+      }
+    } else {
+      print("Geen toestemming om gezondheidsgegevens te lezen.");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -119,7 +188,7 @@ class MetricsSection extends StatelessWidget {
                   Icon(Icons.favorite,
                     color: Colors.red,
                   ),
-                  Text('77', 
+                  Text('$heartrate', 
                     style: TextStyle(
                       color: Colors.red,
                       fontSize: 34,
